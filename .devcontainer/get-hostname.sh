ef3b249bf2d7a0871d4cb765a125e50d7987493c
Manuel de Brito Fontes <aledbf@gmail.com>
2024-04-14 18:04:09 +0200

    Use the default IP address

--- .devcontainer/get-hostname.sh
+++ .devcontainer/get-hostname.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo -n "$(jq -r .workspaceId /usr/local/gitpod/config/spec.json).gitpod.local"
+#echo -n "$(jq -r .workspaceId /usr/local/gitpod/config/spec.json).gitpod.local"
+default_interface=$(ip route show default | awk '/default/ {print $5}')
+default_ip=$(ip addr show $default_interface | awk '/inet / {print $2}' | cut -d/ -f1)
+echo -n "$default_ip.nip.io"
